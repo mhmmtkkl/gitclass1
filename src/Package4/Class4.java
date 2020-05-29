bd139083bac60c8bcdf6434402c9f4fd68e00174
mhmmtkkl <makoklu32@gmail.com>
2020-05-29 01:33:50 -0400

    'secondWeek'

---
+++ src/Package4/Class4.java
@@ -0,0 +1,10 @@
+package Package4;
+
+public class Class4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+
+    }
+}
--- src/package3/class3.java
+++ src/package3/class3.java
@@ -5,6 +5,10 @@ public class class3 {
     public static void main(String[] args) {
 
         System.out.println("push 3");
-        
+
+        /*
+            how do you merge the code in github ?
+                I am creating a pull request in github to merge my code.
+         */
     }
 }
